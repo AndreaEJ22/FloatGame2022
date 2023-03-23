9d94e8c6131ad102238256ccf7454f4675544739
Alzound <64099169+AndreaEJ22@users.noreply.github.com>
2023-03-22 21:54:37 -0600

    shooter

--- FloatGame/Assets/Scripts/Shooter.cs
+++ FloatGame/Assets/Scripts/Shooter.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    public GameObject Bullet;
+    public Transform spawn;
+    public float bulletForce = 5000;
+    public float shootRate = .5f;
+    private float shootRateTime = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,17 @@ public class Shooter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("si");
+            if(Time.deltaTime>shootRateTime)
+            {
+                Debug.Log("operando");
+                GameObject newBullet;
+                newBullet = Instantiate(Bullet, spawn.position, spawn.rotation);
+                newBullet.GetComponent<Rigidbody>().AddForce(spawn.forward * bulletForce);
+                Destroy(newBullet, 2);
+            }
+        }
     }
 }
